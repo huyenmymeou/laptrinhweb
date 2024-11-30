4b60e361a06ff9bce250e7a9c55e0c36e819bf84
Vuhamy2003 <vuhamy2003@gmail.com>
2024-11-30 16:24:08 +0700

    btl v1.0

--- script.js
+++
@@ -1,9 +0,0 @@
-function openFullscreen(fileName) {
-    var img = document.getElementById("fullscreen-img");
-    img.src = "uploads/" + fileName; // Đường dẫn đến ảnh
-    document.getElementById("fullscreen").style.display = "flex"; // Hiện khung ảnh
-}
-
-function closeFullscreen() {
-    document.getElementById("fullscreen").style.display = "none"; // Ẩn khung ảnh
-}
